779f2a9f34e644277a9d32e8ac9d3f27f869c418
NoahWederking <noah376b@stud.kea.dk>
2023-03-22 10:48:24 +0100

    firs commit

---
+++ src/main/java/com/example/basicgui/BasicGuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicGuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicgui/BasicGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
